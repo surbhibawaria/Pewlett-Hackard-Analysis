ce868ae54c2baeb454bbf4180f12f6b0b6b4bd6e
Surbhi Bawaria <surbhibawaria@Surbhis-MBP.attlocal.net>
2022-01-31 17:38:38 -0500

    Updating files

--- schema.sql
+++ schema.sql
@@ -50,6 +50,5 @@ CREATE TABLE titles (
   title varchar(40) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
