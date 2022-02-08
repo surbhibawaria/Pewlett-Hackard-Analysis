6f8c226b54aaa6b3695bba204b9fb75d27692844
Surbhi Bawaria <surbhibawaria@Surbhis-MBP.attlocal.net>
2022-02-08 18:08:38 -0500

    Adding updates

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -21,13 +21,15 @@ order by e.emp_no;
 
 SELECT * FROM retirement_titles;
 
+
 -- Retrieve the employee number, first and last name, and title columns from the Retirement Titles table.
 -- Use the DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows defined by the ON () clause.
 -- Exclude those employees that have already left the company by filtering on to_date to keep only those dates that are equal to '9999-01-01'.
 -- Create a Unique Titles table using the INTO clause.
 -- Sort the Unique Titles table in ascending order by the employee number and descending order by the last date (i.e., to_date) of the most recent title.
 
--- Use Dictinct with Orderby to remove duplicate rows
+-- Use Dictinct with Orderby to remove duplicate rows.
+
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 rt.first_name,
 rt.last_name,
@@ -38,6 +40,7 @@ WHERE (to_date = '9999=01-01')
 ORDER BY emp_no ASC, to_date DESC;
 
 SELECT * FROM unique_titles;
+
 
 -- Write a query to retrieve the number of employees by their most recent job title who are about to retire.
 -- Retrieve the number of titles from the Unique Titles table.
@@ -52,4 +55,27 @@ ORDER BY COUNT(title) DESC;
 
 SELECT * FROM retiring_titles;
 
+-- Deliverable 2
 
+-- Retrieve the emp_no, first_name, last_name, and birth_date columns from the Employees table.
+-- Retrieve the from_date and to_date columns from the Department Employee table.
+-- Retrieve the title column from the Titles table.
+-- Use a DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows defined by the ON () clause.
+-- Create a new table using the INTO clause.
+-- Join the Employees and the Department Employee tables on the primary key.
+-- Join the Employees and the Titles tables on the primary key.
+-- Filter the data on the to_date column to all the current employees, then filter the data on the birth_date columns to get all the employees whose birth dates are between January 1, 1965 and December 31, 1965.
+-- Order the table by the employee number.
+
+SELECT DISTINCT ON (emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, 
+                            de.from_date, de.to_date, t.title
+INTO mentorship_eligibilty
+FROM employees as e
+JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no
+
+SELECT * FROM mentorship_eligibilty;
